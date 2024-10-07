2648df39d8c6b9cb3c14a0ef9cb3dfc104895940
Mona Vishnu <monavishnu@Monas-MacBook-Pro.local>
2024-10-07 15:45:29 +0100

    validation done

--- lib/src/features/workout_screen/presentation/pages/workout_screen.dart
+++ lib/src/features/workout_screen/presentation/pages/workout_screen.dart
@@ -11,6 +11,7 @@ import '../../domain/usecases/update_workout_usecase.dart';
 import '../bloc/workout_bloc.dart';
 
 import '../bloc/workout_event.dart';
+import '../bloc/workout_state.dart';
 import 'workout_list.dart';
 
 class WorkoutScreen extends StatefulWidget {
@@ -81,6 +82,24 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
     }
   }
 
+  goToWorkoutListScreen() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final workoutRepository =
+        SharedPreferencesWorkoutRepository(sharedPreferences);
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => BlocProvider(
+        create: (context) => WorkoutBloc(
+          AddWorkout(workoutRepository),
+          UpdateWorkout(workoutRepository),
+          DeleteWorkout(workoutRepository),
+          GetWorkouts(workoutRepository),
+        ),
+        child: const WorkoutList(),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,144 +126,179 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Day Dropdown
-              DropdownButtonFormField(
-                value: _selectedDay,
-                decoration: InputDecoration(
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey[400]!),
-                  ),
-                  labelText: 'Select Day',
-                  filled: true,
-                  fillColor: Colors.grey[100],
+        child: BlocListener<WorkoutBloc, WorkoutState>(
+          listener: (context, state) {
+            if (state is WorkoutLoaded) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(widget.workout == null
+                      ? 'Workout added successfully!'
+                      : 'Workout updated successfully!'),
+                  duration: const Duration(seconds: 2),
                 ),
-                items: dayItems.map((dayItem) {
-                  return DropdownMenuItem(
-                    value: dayItem.id,
-                    child: Text(dayItem.day),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedDay = value!;
-                  });
-                },
-                validator: (value) {
-                  if (value == null) return 'Please select a day';
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              // Exercise Dropdown
-              DropdownButtonFormField<String>(
-                value: _selectedExercise,
-                decoration: InputDecoration(
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 195, 188, 188)!),
-                  ),
-                  labelText: 'Select Exercise',
-                  filled: true,
-                  fillColor: Colors.grey[100],
-                ),
-                items: _exercises.map((exercise) {
-                  return DropdownMenuItem(
-                    value: exercise,
-                    child: Text(exercise),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedExercise = value!;
-                  });
-                },
-                validator: (value) {
-                  if (value == null) return 'Please select an exercise';
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              // Weight Input Field
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Weight (kg)',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[100],
-                ),
-                keyboardType: TextInputType.number,
-                onSaved: (value) {
-                  _weight = double.parse(value!);
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty)
-                    return 'Please enter weight';
-                  return null;
-                },
-                initialValue: widget.workout != null ? _weight.toString() : '',
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Repetitions',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[100],
-                ),
-                keyboardType: TextInputType.number,
-                onSaved: (value) {
-                  _repetitions = int.parse(value!);
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter repetitions';
-                  }
-                  return null;
-                },
-                initialValue:
-                    widget.workout != null ? _repetitions.toString() : '',
-              ),
-              const SizedBox(height: 42),
-              Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 45, 87, 121),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 12), // Padding
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(10), // Rounded corners
+              );
+              Future.delayed(const Duration(milliseconds: 1000), () {
+                goToWorkoutListScreen();
+              });
+            }
+          },
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Day Dropdown
+                DropdownButtonFormField(
+                  value: _selectedDay,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey[400]!),
                     ),
-                    elevation: 5, // Elevation for shadow effect
+                    labelText: 'Select Day',
+                    filled: true,
+                    fillColor: Colors.grey[100],
                   ),
-                  onPressed: _addSet,
-                  child: Text(
-                    widget.workout == null
-                        ? 'Add Workout Set'
-                        : 'Edit Workout Set',
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
+                  items: dayItems.map((dayItem) {
+                    return DropdownMenuItem(
+                      value: dayItem.id,
+                      child: Text(dayItem.day),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedDay = value!;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null) return 'Please select a day';
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                // Exercise Dropdown
+                DropdownButtonFormField<String>(
+                  value: _selectedExercise,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                          color: Color.fromARGB(255, 195, 188, 188)),
+                    ),
+                    labelText: 'Select Exercise',
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                  ),
+                  items: _exercises.map((exercise) {
+                    return DropdownMenuItem(
+                      value: exercise,
+                      child: Text(exercise),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedExercise = value!;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null) return 'Please select an exercise';
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                // Weight Input Field
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Weight (kg)',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSaved: (value) {
+                    _weight = double.parse(value!);
+                  },
+                  validator: (value) {
+                    // Check if the input is empty
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter weight';
+                    }
+
+                    // Check if the input is a valid integer
+                    final doubleValue = double.tryParse(value);
+                    if (doubleValue == null || doubleValue <= 0) {
+                      return 'Please enter a valid positive number';
+                    }
+
+                    return null; // Return null if validation passes
+                  },
+                  initialValue:
+                      widget.workout != null ? _weight.toString() : '',
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Repetitions',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSaved: (value) {
+                    _repetitions = int.parse(value!);
+                  },
+                  validator: (value) {
+                    // Check if the input is empty
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter repetitions';
+                    }
+
+                    // Check if the input is a valid integer
+                    final intValue = int.tryParse(value);
+                    if (intValue == null || intValue <= 0) {
+                      return 'Please enter a valid positive number';
+                    }
+
+                    return null; // Return null if validation passes
+                  },
+                  initialValue:
+                      widget.workout != null ? _repetitions.toString() : '',
+                ),
+                const SizedBox(height: 42),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 45, 87, 121),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12), // Padding
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10), // Rounded corners
+                      ),
+                      elevation: 5, // Elevation for shadow effect
+                    ),
+                    onPressed: _addSet,
+                    child: Text(
+                      widget.workout == null
+                          ? 'Add Workout Set'
+                          : 'Edit Workout Set',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
